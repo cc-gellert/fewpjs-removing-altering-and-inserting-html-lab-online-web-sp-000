a788dcc956906fba2d67b58228f7a52c4e5d119a
Chloe Gell <gellert.chloe@gmail.com>
2020-02-27 19:24:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,2 @@
 // Write your code here!
-let element = document.querySelector("p#greeting");
-element.innerHTML = 'Hello, DOM!'
-let ul = document.createElement('ul'); 
-for(let i = 0; i < 3; i++){
-  let li = document.createElement('li')
-  li.innerHTML = (i + 1).toString()
-  ul.appendChild(li)
-}
-
-element.appendChild(ul); 
+document.removeChild('')
