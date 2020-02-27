d6a5f05b44701aa4174d27a488ae090f01232341
Chloe Gell <gellert.chloe@gmail.com>
2020-02-27 19:37:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ main.remove();
 
 let newHeader = document.createElement('h1'); 
 document.body.appendChild(newHeader); 
+newHeader.id = "victory"; 
+newHeader.innerHTML = "YOUR-NAME is the champion"; 
